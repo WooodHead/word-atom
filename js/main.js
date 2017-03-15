cd039bd002ae7d2676acc67d9f7c326464439fdb
Wushaowei001 <jcwsw129@gmail.com>
2017-03-15 20:07:15 +0800

    random show word and definition

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+    var index;
+    var h1 = document.getElementById("word");
+    var ul = document.getElementById("definition");
 
+    var size = wds.length;
+    var defs = [];
+
+    function updateDefinition() {
+
+        ul.innerHTML = "";
+        defs = wds[index].slice(1);
+        defs.forEach(function(val, index) {
+            ul.innerHTML += "<li>" + val + "</li>";
+        });
+
+        setTimeout(updateWord, 1000);
+
+    }
+
+    function updateWord() {
+        index = Math.floor(Math.random() * size);
+
+        h1.innerHTML = wds[index][0];
+        ul.innerHTML = "";
+
+        setTimeout(updateDefinition, 1000);
+    }
+
+    setTimeout(updateWord, 1000);
+});
